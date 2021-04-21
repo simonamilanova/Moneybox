63ed15bd658a5501b13c1b0dabc1745ae9940d44
simonamilanova <smilanova97@gmail.com>
2021-04-21 23:37:41 +0200

    Project finished

---
+++ script.js
@@ -0,0 +1,73 @@
+//Change header color on scroll
+let header = document.querySelector(".main-header");
+
+window.addEventListener("scroll", () => {
+    let scrollPosition = window.scrollY;
+    if(scrollPosition > 10) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
+
+
+//To top button 
+let btnToTop = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btnToTop.style.display = "block";
+  } else {
+    btnToTop.style.display = "none";
+  }
+};
+
+btnToTop.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
+
+
+//Handling prices
+$(document).ready(function () {
+  let baseUrl2 = "http://api.ipstack.com/77.28.14.118?access_key=ad5de90628b7cd643ce58de917cdf4fe";
+  let premiumVal = document.getElementById("premiumValue");
+  let businessVal = document.getElementById("businessValue");
+  let element = $(".contentBasicPlan");
+
+  window.addEventListener("load", () => {
+    $.ajax ({
+      url: baseUrl2,
+      success: function (response) {
+        if(response.continent_name === "Europe") {
+          businessVal.textContent = "€";
+          premiumVal.textContent = "€";
+        } else if (response.country_code === "GBR" && response.continent_name === "Europe") {
+          businessVal.textContent = "£ ";
+          premiumVal.textContent = "£ ";
+        } else {
+          businessVal.textContent = "$";
+          premiumVal.textContent = "$";
+        }
+      },
+      error: function (error) {
+        console.error("Something went wrong");
+      }
+    })
+  })
+
+  $(window).resize(function() {
+    if ($(window).width() < 400) {
+      $(element).hide();
+
+    } else if ($(window).width() > 1000) {
+      $(element).show();
+    }
+  });
+
+});
+
+
+
+
